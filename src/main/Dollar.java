b9e358d8bf47cd17e93c573045b166b3ff9c2765
JaeSeo Yang <qud1251@gmail.com>
2023-07-11 16:03:32 +0900

    first commit

---
+++ src/main/Dollar.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * @sample [src\test\main\DollarTest.java]
+ */
+public class Dollar {
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        amount *= multiplier;
+    }
+}
+
+
+
---
+++ src/test/main/DollarTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DollarTest {
+    @Test
+    void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
