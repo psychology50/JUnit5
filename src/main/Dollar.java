30a0ebaaaf6de5da8be7d918b181b6d4ad7ef40d
JaeSeo Yang <qud1251@gmail.com>
2023-08-01 10:50:02 +0900

    ch 1-4

--- src/main/Dollar.java
+++ src/main/Dollar.java
@@ -4,7 +4,7 @@ package main;
  * @sample [src\test\main\DollarTest.java]
  */
 public class Dollar {
-    int amount;
+    private int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
@@ -12,6 +12,11 @@ public class Dollar {
 
     public Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
+    }
+
+    @Override public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
     }
 }
 
